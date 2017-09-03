24f9372bc2989fefbe875bbc9360dfbbcacc7e18
ChintamaniKoli <30986766+ChintamaniKoli@users.noreply.github.com>
2017-09-03 06:31:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -119,7 +119,7 @@ app.get('/articles/:articleName', function (req, res) {
 //articles[articleName] = {} content object for article one
 var articleName = req.params.articleName;
 
-pool.query("Select * FROM article Where title = '" + req.params.articleName + "'", function (req, res){ 
+pool.query("SELECT * FROM article WHERE title = '" + req.params.articleName + "'", function (req, res){ 
         if (err) { res.status(500).send(err.toString());
         } else {
             if (result.rows.length === 0) {
