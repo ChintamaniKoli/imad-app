d0169f98b5a37d965242fbc77c4dd7854e4e57bb
ChintamaniKoli <30986766+ChintamaniKoli@users.noreply.github.com>
2017-09-03 06:46:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -131,9 +131,7 @@ app.get('/articles/:articleName', function (req, res) {
                             res.send(createTemplate(articleData));
                         }
                 }
-        
-});
-    
+    });
 });
 
 
