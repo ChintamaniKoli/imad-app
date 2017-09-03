ca85d65ea34213dfd9a24493ceeb56407fb1f48f
ChintamaniKoli <30986766+ChintamaniKoli@users.noreply.github.com>
2017-09-03 06:36:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -119,7 +119,8 @@ app.get('/articles/:articleName', function (req, res) {
 //articles[articleName] = {} content object for article one
 
 pool.query("SELECT * FROM article WHERE title = '" + req.params.articleName + "'", function (req, res) { 
-        if (err) { res.status(500).send(err.toString());
+        if (err) {
+            res.status(500).send(err.toString());
         } else {
             if (result.rows.length === 0) {
                 rest.status(404).send('Record not Found');
